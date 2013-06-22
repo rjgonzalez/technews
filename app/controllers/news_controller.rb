d02c4d83588e8e9d66c15502737dfa3ca8c3a757
Ramon Gonzalez <rjgt04@gmail.com>
2013-06-22 19:02:21 -0400

    Done. Freaking bitly solved the space problem about sendgrid. Thank you Justin for the idea.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 class NewsController < ApplicationController
   # GET /news
   # GET /news.json
@@ -42,8 +43,56 @@ class NewsController < ApplicationController
   def create
     @news = News.new(params[:news])
 
+    @people = Signup.all
+
     respond_to do |format|
       if @news.save
+
+        @people.each do |e|
+
+          emailAddress = e.email
+          # description = @news.description.gsub(' ','%2520')
+          # title = @news.title.gsub(' ','%2520')
+
+          encoded1 = URI::encode(@news.description)
+          encoded2 = URI::encode(@news.title)
+
+          description = encoded1.gsub(' ','%%%20')
+          title = encoded2.gsub(' ','%%%20')
+          ap title
+
+        
+          # description = encoded1
+          # title = encoded2
+
+          #Set email content. 
+          subject = "More%20local%20tech%20news"
+          footer = "%0D%0DThanks,%0D%0D%5F%5F%0D%0DOur%20Tech%20Hub"
+
+          #links
+          bitly = Bitly.new('rjgonzo','R_e5e13a8afae46931a3091994f4b844d7')
+          twitter = "https://twitter.com/intent/tweet?text=#{description}&url=#{@news.url}"
+          twitter_url = bitly.shorten(twitter)
+          twitter_final = twitter_url.shorten
+
+          facebook = "http://www.facebook.com/sharer.php?s=100&p[url]=#{@news.url}&p[title]=#{title}&p[summary]=#{description}"
+          fb_url = bitly.shorten(facebook)
+          fb_final = fb_url.shorten
+
+          message = "%0D%0D#{title}%0D%0D#{description}%0D%0DShare:%0D%0DFacebook:%20#{fb_final}%0D%0DTwitter:%20#{twitter_final}"
+          # message = "%0D%0D#{title}%0D%0D#{description}%0D%0DShare:%0D%0Dhttps://twitter.com/intent/tweet?text=#{description}%20#{@news.url}"
+          email = "https://sendgrid.com/api/mail.send.json?api_user=rgonzalez&api_key=123456&to=#{emailAddress}&toname=Caca%20Stranger&subject=#{subject}&text=#{message}&from=news@ourtechhub.com&fromname=Our%20Tech%20Hub"
+
+          #Send email to request signature (with Sendgrid)
+
+          # encoded = URI::encode(email)
+          ap 'Email Data'
+          ap email
+          # ap 'caca'
+          # ap encoded
+          HTTParty.get(email)
+      end
+
         format.html { redirect_to @news, notice: 'News was successfully created.' }
         format.json { render json: @news, status: :created, location: @news }
       else
