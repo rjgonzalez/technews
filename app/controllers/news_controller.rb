1c7b90e809b693ccdc4f9686917832b9c9aaf64e
Ramon Gonzalez <rjgt04@gmail.com>
2013-06-24 19:15:38 -0400

    no chanegs

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -84,7 +84,7 @@ class NewsController < ApplicationController
           message = "#{header}%0D%0DTitle:%0D%0D#{title}%0D%0DDescription:%0D%0D#{description}%0D%0D#{@news.url}%0D%0DSent%20by:%20#{name},%20#{twitterName}%0D%0D#{share}%0D%0DFacebook:%20#{fb_final}%20,%20Twitter:%20#{twitter_final}%0D%0D#{unsubscribe}"
           email = "https://sendgrid.com/api/mail.send.json?api_user=rgonzalez&api_key=123456&to=#{emailAddress}&toname=Influencer&subject=#{subject}&text=#{message}&from=#{@news.email}&fromname=#{name}"
 
-          #Send email to request signature (with Sendgrid)
+          #Send email to request signature (with Sendgrid)  
           ap 'Email Data'
           ap email
           HTTParty.get(email)
